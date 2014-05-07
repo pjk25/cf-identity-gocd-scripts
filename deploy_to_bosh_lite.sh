afbfa5c4fd2036352685f31b1d385ce8305706ae
Filip Hanik and Philip Kuryloski <pair+fhanik+pkuryloski@pivotallabs.com>
2014-05-07 11:32:20 -0700

    Add bosh deployment script

---
+++ deploy_to_bosh_lite.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+./update
+cp /var/vcap/packages/scripts/cf-stub.yml
+rm -f ~/.bosh_config
+bosh -u admin -p admin target $BOSH_LITE_TARGET
+../gocd_scripts/replace_director_uuid.sh
+../gocd_scripts/generate_warden_manifest.sh
+bosh -n create release cf --force
+bosh login admin admin
+bosh -n upload release
+bosh -n upload stemcell http://bosh-jenkins-gems-warden.s3.amazonaws.com/stemcells/latest-bosh-stemcell-warden.tgz --skip-if-exits
+bosh deployment the_manifest.yml
+bosh -n deploy
