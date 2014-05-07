356ccbda579c644941ff66716162c634ee693363
Filip Hanik and Philip Kuryloski <pair+fhanik+pkuryloski@pivotallabs.com>
2014-05-07 11:44:02 -0700

    Allow bosh release upload to skip if release already exists

--- deploy_to_bosh_lite.sh
+++ deploy_to_bosh_lite.sh
@@ -10,7 +10,7 @@ bosh -u admin -p admin target $BOSH_LITE_TARGET
 ../gocd_scripts/generate_warden_manifest.sh
 bosh -n create release cf --force
 bosh login admin admin
-bosh -n upload release
+bosh -n upload release --skip-if-exists
 bosh -n upload stemcell http://bosh-jenkins-gems-warden.s3.amazonaws.com/stemcells/latest-bosh-stemcell-warden.tgz --skip-if-exits
 bosh deployment the_manifest.yml
 bosh -n deploy
