f805633b84d0735e50ed0b6f8741a540dc731410
Filip Hanik and Philip Kuryloski <pair+fhanik+pkuryloski@pivotallabs.com>
2014-05-07 11:48:03 -0700

    Fix typo

--- deploy_to_bosh_lite.sh
+++ deploy_to_bosh_lite.sh
@@ -11,6 +11,6 @@ bosh -u admin -p admin target $BOSH_LITE_TARGET
 bosh -n create release cf --force
 bosh login admin admin
 bosh -n upload release --skip-if-exists
-bosh -n upload stemcell http://bosh-jenkins-gems-warden.s3.amazonaws.com/stemcells/latest-bosh-stemcell-warden.tgz --skip-if-exits
+bosh -n upload stemcell http://bosh-jenkins-gems-warden.s3.amazonaws.com/stemcells/latest-bosh-stemcell-warden.tgz --skip-if-exists
 bosh deployment the_manifest.yml
 bosh -n deploy
