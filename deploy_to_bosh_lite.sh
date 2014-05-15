322442027aca4052393342086e35068e7e0c0131
Philip Kuryloski <pair+pkuryloski@pivotallabs.com>
2014-05-14 17:25:07 -0700

    reorder login step

--- deploy_to_bosh_lite.sh
+++ deploy_to_bosh_lite.sh
@@ -8,9 +8,9 @@ rm -f ~/.bosh_config
 bosh -u admin -p admin target $BOSH_LITE_TARGET
 ../gocd_scripts/replace_director_uuid.sh
 ../gocd_scripts/generate_warden_manifest.sh
-bosh -n delete release cf
 bosh -n create release cf --force
 bosh login admin admin
+bosh -n delete release cf
 bosh -n upload release --skip-if-exists
 bosh -n upload stemcell http://bosh-jenkins-gems-warden.s3.amazonaws.com/stemcells/latest-bosh-stemcell-warden.tgz --skip-if-exists
 bosh deployment the_manifest.yml
